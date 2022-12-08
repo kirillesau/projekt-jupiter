7dcdccce9f64bff2f58b581ee95419aa85ff904b
kirill <kirill-esau@outlook.de>
2022-12-08 20:27:24 +0100

    Header und Hauptmenü Komponenten hinzugefügt

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AppComponent} from "./app.component";
+import {MainMenuComponent} from "./main-menu/main-menu.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainMenuComponent, pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
